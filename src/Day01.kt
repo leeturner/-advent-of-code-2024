7aea49f2eb0b5e7589ba79266a1b4988e7376a05
leeturner <lee@leeturner.me>
2024-12-01 09:51:48 +0000

    Day 1 of advent of code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,26 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftLocationIds = input.toSortedLeftList()
+        val rightLocationIds = input.toSortedRightList()
+        return leftLocationIds.zip(rightLocationIds).sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftLocationIds = input.toSortedLeftList()
+        val rightLocationIds = input.toSortedRightList()
+        return leftLocationIds.sumOf { a -> rightLocationIds.count { it == a } * a }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+fun List<String>.toSortedLeftList() = map { it.substringBefore(" ").toInt() }.sorted()
+fun List<String>.toSortedRightList() = map { it.substringAfterLast(" ").toInt() }.sorted()
